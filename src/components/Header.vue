36e72897ca0770e919388cf0be9e568fba9ed216
Joseph Infante <josephinfante.y@gmail.com>
2022-01-15 23:49:12 -0500

    Adding styles for tag <a> on hover

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,8 +3,8 @@
     <div class="container mx-auto px-5 grid grid-cols-2 gap-4">
       <div>Logo</div>
       <div class="flex justify-end">
-        <a href="" class="mx-3">Iniciar sesión</a>
-        <a href="" class="mx-3">Registrarse</a>
+        <a href="" class="mx-3 my-auto links">Iniciar sesión</a>
+        <a href="" class="mx-3 my-auto links">Registrarse</a>
         <label class="switch ml-3">
           <input type="checkbox">
           <span class="slider round"></span>
@@ -21,63 +21,66 @@ export default {
 </script>
 
 <style>
-.switch {
-  position: relative;
-  display: inline-block;
-  width: 60px;
-  height: 34px;
-}
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 60px;
+    height: 34px;
+  }
 
-.switch input { 
-  opacity: 0;
-  width: 0;
-  height: 0;
-}
+  .switch input { 
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
 
-.slider {
-  position: absolute;
-  cursor: pointer;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: #FCFC62;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #FCFC62;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
 
-.slider:before {
-  position: absolute;
-  content: "";
-  height: 26px;
-  width: 26px;
-  left: 4px;
-  bottom: 4px;
-  background-color: white;
-  -webkit-transition: .4s;
-  transition: .4s;
-}
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: .4s;
+    transition: .4s;
+  }
 
-input:checked + .slider {
-  background-color: #2D3047;
-}
+  input:checked + .slider {
+    background-color: #2D3047;
+  }
 
-input:focus + .slider {
-  box-shadow: 0 0 1px #2D3047;
-}
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2D3047;
+  }
 
-input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
-}
+  input:checked + .slider:before {
+    -webkit-transform: translateX(26px);
+    -ms-transform: translateX(26px);
+    transform: translateX(26px);
+  }
 
-/* Rounded sliders */
-.slider.round {
-  border-radius: 34px;
-}
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
 
-.slider.round:before {
-  border-radius: 50%;
-}
+  .slider.round:before {
+    border-radius: 50%;
+  }
+  .links:hover {
+    color: #FCFC62;
+  }
 </style>
