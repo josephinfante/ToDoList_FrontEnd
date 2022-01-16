394507c723f3427d25183fc255354e173a30f106
Joseph Infante <josephinfante.y@gmail.com>
2022-01-15 23:48:05 -0500

    Adding default font for elements

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Header />
-    <div class="bg-red-500 container mx-auto min-h-screen text-center">
+    <div class="container mx-auto min-h-screen text-center pt-10">
       <router-view/>
     </div>
   </div>
@@ -21,6 +21,24 @@ export default {
 <style>
 @font-face {
   font-family: 'Poppins-regular';
-  src: local('Poppins-regular'), url('./assets/fonts/Poppins-Regular.ttf');
+  src: local('Poppins-regular'), url('./assets/fonts/Poppins/Poppins-Regular.ttf');
+}
+@font-face {
+  font-family: 'Poppins-medium';
+  src: local('Poppins-medium'), url('./assets/fonts/Poppins/Poppins-Medium.ttf');
+}
+@font-face {
+  font-family: 'Poppins-semibold';
+  src: local('Poppins-semibold'), url('./assets/fonts/Poppins/Poppins-SemiBold.ttf');
+}
+@font-face {
+  font-family: 'Poppins-black';
+  src: local('Poppins-black'), url('./assets/fonts/Poppins/Poppins-Black.ttf');
+}
+* {
+  font-family: 'Poppins-regular';
+}
+h1 {
+  font-family: 'Poppins-black';
 }
 </style>
