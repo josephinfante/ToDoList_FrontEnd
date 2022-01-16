84a4589fcb0c43688881c6f6e7751ce5070991bf
Joseph Infante <josephinfante.y@gmail.com>
2022-01-16 00:34:02 -0500

    Creating Input component

---
+++ src/components/Input.vue
@@ -0,0 +1,28 @@
+<template>
+    <input class="input my-4" :type="type" :placeholder="placeholder">
+</template>
+
+<script>
+export default {
+    name: 'Input',
+    props: {
+        type: String,
+        placeholder: String
+    }
+}
+</script>
+
+<style lang="css">
+    .input {
+        background: #E5E5E5;
+        width: 300px;
+        height: 50px;
+        border-radius: 50px;
+        color: #747474;
+        padding: 0px 25px;
+        font-size: 12px;
+    }
+    .input:focus-visible {
+        outline: none;
+    }
+</style>
