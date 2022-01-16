bc6542596c69a79df2ad6e8ab22ff71b05087423
Joseph Infante <josephinfante.y@gmail.com>
2022-01-16 01:49:16 -0500

    Creating digits input

---
+++ src/components/InputDigits.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="inputDigits flex justify-center my-10">
+        <input maxlength="1" type="text" class="digits mx-2">
+        <input maxlength="1" type="text" class="digits mx-2">
+        <input maxlength="1" type="text" class="digits mx-2">
+        <input maxlength="1" type="text" class="digits mx-2">
+        <input maxlength="1" type="text" class="digits mx-2">
+        <input maxlength="1" type="text" class="digits mx-2">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'InputDigits',
+    method: {
+    },
+}
+</script>
+
+<style lang="css">
+    .digits {
+        width: 45px;
+        border-bottom: 4px solid #E5E5E5;
+        text-align: center;
+        color: #747474;
+    }
+    .digits:focus-visible {
+        outline: none;
+    }
+</style>
