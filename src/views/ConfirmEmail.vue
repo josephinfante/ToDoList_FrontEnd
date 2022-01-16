ae9f5bb3ee3d63bd9de100fc0ee6175c63e2b8d7
Joseph Infante <josephinfante.y@gmail.com>
2022-01-16 01:49:39 -0500

    Creating confirm email view

---
+++ src/views/ConfirmEmail.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="confirmEmail">
+        <Titles name="Confirma tu correo"/>
+        <InputDigits />
+        <Button btn_text="Confirmar correo"/>
+    </div>
+</template>
+
+<script>
+import Titles from '@/components/Titles';
+import Button from '@/components/Button';
+import InputDigits from '@/components/InputDigits';
+
+export default {
+    name: 'ConfirmEmail',
+    components : {
+        Titles,
+        Button,
+        InputDigits
+    },
+}
+</script>
+
+<style lang="css">
+    
+</style>
