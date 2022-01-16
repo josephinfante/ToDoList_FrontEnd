eed6afceb54cf2f5be18012dfd85c01cc838479f
Joseph Infante <josephinfante.y@gmail.com>
2022-01-16 00:34:19 -0500

    Creating title component

---
+++ src/components/Titles.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1 class="title">{{ name }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Titles',
+    props:  {
+        name: String
+    }
+}
+</script>
+
+<style lang="css">
+    .title {
+        font-size: 40px;
+    }
+</style>
