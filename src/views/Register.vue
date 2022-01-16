058eed4c65c9cadcbec2fb9862e741221e8d0833
Joseph Infante <josephinfante.y@gmail.com>
2022-01-16 01:50:06 -0500

    Adding routing to the register button

---
+++ src/views/Register.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="register">
+        <Titles name="Registrate"/>
+        <div v-for="i in inputs" :key="i.placeholder">
+            <Input :type="i.type" :placeholder="i.placeholder"/>
+        </div>
+        <Button btn_link="ConfirmEmail" btn_text="Registrarme"/>
+    </div>
+</template>
+
+<script>
+import Titles from '@/components/Titles';
+import Input from '@/components/Input';
+import Button from '@/components/Button';
+
+export default {
+    name: 'Register',
+    components : {
+        Titles,
+        Input,
+        Button
+    },
+    data() {
+        return {
+            inputs: [
+                 {
+                    type: 'text',
+                    placeholder: 'Nombre'
+                },
+                {
+                    type: 'email',
+                    placeholder: 'Correo electrónico'
+                },
+                {
+                    type: 'password',
+                    placeholder: 'Contraseña'
+                },
+                {
+                    type: 'password',
+                    placeholder: 'Confirmar contraseña'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="css">
+    
+</style>
